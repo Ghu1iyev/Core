e9e77e273355d3dffb150b10b8b0b37bd0807463
Ghu1iyev <tu12n4c1r@code.edu.az>
2023-09-17 17:32:49 +0400

    ceate folder structure

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "violets-are-blue": "#8E59FF",
+        "bittersweet": "#FF6A55",
+        "dollar-bill": "#83BF6E",
+        "dodger-blue": "#2A85FF"
+      }
+    },
+  },
+  plugins: [],
+}
