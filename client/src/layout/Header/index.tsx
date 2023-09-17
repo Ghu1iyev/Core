c32e4959111c19db9a02c5f137f2087dceb9c9be
Ghu1iyev <tu12n4c1r@code.edu.az>
2023-09-17 23:46:33 +0400

    header

--- client/src/layout/Header/index.tsx
+++ client/src/layout/Header/index.tsx
@@ -1,7 +1,25 @@
+import {RiSearch2Line} from 'react-icons/ri'
+import {PiPlusBold} from 'react-icons/pi'
+import styled from './header.module.css'
+import Messageicon from '../../assets/images/Message.svg'
+import NotifictaionIcon from '../../assets/images/Notification.svg'
+import Avatar from '../../assets/images/Avatar.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styled.header}>
+        <div className={styled.search}>
+          <RiSearch2Line color={"#6F767E"} fontSize={22}/>
+          <input type="text" placeholder="Search or type a command" />
+          <span>⌘ F</span>
+        </div>
+        <div className='flex gap-6'>
+          <button className={styled.create_btn}><PiPlusBold fontSize={20}/>Create</button>
+          <img src={Messageicon} alt="" />
+          <img src={NotifictaionIcon} alt="" />
+          <img src={Avatar} alt="" />
+        </div>
+    </div>
   )
 }
 
